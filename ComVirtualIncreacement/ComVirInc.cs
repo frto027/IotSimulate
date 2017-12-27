6e601b54119e4c6b36906e15945e39f240b4e21b
胡宇浩 <602706150@qq.com>
2017-12-27 14:52:44 +0800

    增加ComVirtualIncreacement类，每次调用SendData自动发送一个数字出去，用于调试。用户界面IotSimulate正在琢磨，不太熟WPF，还没写。

---
+++ ComVirtualIncreacement/ComVirInc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IoTSimulate;
+
+namespace ComVirtualIncreacement
+{
+    public class ComVirInc : ComBase
+    {
+        byte s = 0;
+        public override void OnDataReceive(byte[] data, int offset, int len)
+        {
+            Console.WriteLine("Data receive by Vir Com");
+        }
+
+        public void SendData()
+        {
+            ToConnector(new byte[] { s++ }, 0, 1);
+        }
+    }
+}
